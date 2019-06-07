d47f90aeff168d2824f583caa4476981c482a4b1
williamf1 <45521699+williamf1@users.noreply.github.com>
2019-06-07 16:54:11 +1000

    update v.1

---
+++ timestables.py
@@ -0,0 +1,158 @@
+import time
+
+
+#start
+print("""   
+
+                             o o
+   o      o           o      o  o
+  o  o   o o        o        o   o
+ o     o    o      o         o   o
+o            o       o       o  o
+                       o     o o     (for multiplication)
+
+""")
+
+
+#login
+needspassword=True
+while needspassword==True:
+
+  #username/password input
+  username=raw_input("username:")
+  password=raw_input("password:")
+
+#repeat
+  if username!="m c d" or password!="maths cheating device":
+    print("   ")
+    print ("incorrect username/password")
+  else:
+    needspassword=False
+  
+
+
+
+#introduction
+print("  ")
+
+print("welcome to the maths cheating device (for multiplication) ")
+
+print("  ")
+
+
+#how to
+time.sleep(1)
+print("follow the instructions to get your answers")
+
+print("  ")
+
+
+
+
+#contact
+time.sleep(1)
+print("if you need help contact:ken.faulkner.gh@gmail.com")
+
+print("  ")
+
+time.sleep(1)
+
+
+
+
+
+#input for calculator
+timest=raw_input("what timestable do you want?")
+
+loop=raw_input("up to what number?")
+
+
+
+#waiting to give answers
+print("calculating. ")
+time.sleep(0.5)
+print("calculating.. ")
+time.sleep(0.5)
+print("calculating... ")
+time.sleep(0.5)
+print("calculating. ")
+time.sleep(0.5)
+print("calculating.. ")
+time.sleep(0.5)
+print("calculating... ")
+time.sleep(1)
+print("finished")
+time.sleep(1)
+
+
+
+
+#calculator
+inttimest=int(timest)
+
+for x in range(int(loop)+1) :
+  answer=inttimest*x
+
+  print(str(x)+"x"+timest+"="+str(answer))
+
+
+
+
+
+  
+#for emergency contact email william.faulkner@spcc.nsw.edu.au
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
