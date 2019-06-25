f907b06229737cd25e36e3b73b462719a5e23057
willfau <ken.faulkner.gh@gmail.com>
2019-06-25 19:03:52 +1000

    new long multiplication

---
+++ long multiplication.py
@@ -0,0 +1,36 @@
+import time
+
+print("test multi")
+
+
+
+#input
+no1=raw_input("first number")
+
+no2=raw_input("times by")
+
+
+
+
+
+#calculator
+no1int=int(no1)
+
+
+no2int=int(no2)
+
+answer=no1int * no2int
+
+
+#printing
+print("in long multiplication")
+
+print("   ")
+
+print(str(no1)+" x")
+
+print(str(no2))
+
+print("-----") 
+
+print(str(answer))
